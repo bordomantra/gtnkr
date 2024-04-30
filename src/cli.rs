53c258a42ee581b42dfc0e875443fa6ae8d06b6e
bordomantra <contact@mail.bordomantra.me>
2024-04-30 09:24:46 +0200

    Switched to clap for the CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,43 +1,69 @@
 use crate::game_launcher::{GameLauncher, GameLauncherError};
+use clap::{Parser, Subcommand};
 use regex::Regex;
 
+#[derive(Parser)]
+#[command(version, about)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: SubCommands,
+}
+
+#[derive(Subcommand)]
+enum SubCommands {
+    Launch {
+        #[arg(value_parser = launch_subcommand_parser)]
+        game: GameArgument,
+    },
+}
+
+#[derive(Clone)]
+enum GameArgument {
+    Executable(String),
+    SteamLaunchCommand(String, u32),
+}
+
+fn launch_subcommand_parser(string: &str) -> Result<GameArgument, String> {
+    // TODO: This regex could be improved, it's very easy to trick it.
+    let steam_command_regex =
+        Regex::new(r#"SteamLaunch AppId=(\d+)"#).expect("Failed to compile the regex");
+
+    if let Ok(executable_path) = which::which(string) {
+        if let Some(path_string) = executable_path.to_str() {
+            return Ok(GameArgument::Executable(path_string.to_string()));
+        }
+
+        return Err(String::from("Provided argument is a valid executable, but I wasn't able to convert it's path to string."));
+    }
+
+    if let Some(steam_app_id) = steam_command_regex.captures(string).and_then(|captures| {
+        captures
+            .get(1)
+            .and_then(|r#match| r#match.as_str().parse::<u32>().ok())
+    }) {
+        return Ok(GameArgument::SteamLaunchCommand(
+            string.to_string(),
+            steam_app_id,
+        ));
+    }
+
+    Err(String::from(
+        "Provided argument is neither a valid executable or a Steam launch %command%",
+    ))
+}
+
 pub async fn run() -> Result<(), GameLauncherError> {
-    let mut arguments: Vec<String> = std::env::args().collect();
+    let commands = Cli::parse();
 
-    arguments.remove(0); // Current directory's path, we don't need that.
-
-    let argument_as_string = arguments.join(" ");
-
-    let steam_app_id_regex =
-        Regex::new(r#"SteamLaunch AppId=(\d+)"#).expect("Failed to compile regex");
-
-    if let Some(steam_app_id) =
-        steam_app_id_regex
-            .captures(&argument_as_string)
-            .and_then(|captures| {
-                captures
-                    .get(1)
-                    .and_then(|r#match| r#match.as_str().parse::<u64>().ok())
-            })
-    {
-        GameLauncher::launch_by_command(&argument_as_string, &format!("{steam_app_id}.ron"))
-            .await?;
-
-        return Ok(());
+    match &commands.subcommand {
+        SubCommands::Launch { game } => match game {
+            GameArgument::Executable(path_string) => {
+                GameLauncher::launch_by_executable(path_string).await
+            }
+            GameArgument::SteamLaunchCommand(command, steam_app_id) => {
+                GameLauncher::launch_by_command(command, &format!("{steam_app_id}.ron")).await
+            }
+        },
     }
-
-    if let Ok(executable_path) = which::which(argument_as_string) {
-        GameLauncher::launch_by_executable(
-            executable_path
-                .file_name()
-                .expect("Failed to get the file name")
-                .to_str()
-                .expect("Failed to convert into string"),
-        )
-        .await?;
-
-        return Ok(());
-    }
-
-    Ok(())
 }
